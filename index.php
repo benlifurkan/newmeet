86713f16811c593fe8b74fed5b4c2876fa56192d
benlifurkan <furkanbenli33@gmail.com>
2024-02-08 18:15:19 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Yeni insanlarla tanışmak için hemen üye ol!">
+    <meta name="keywords" content="tanışma, arkadaşlık, flört, yeni insanlarla tanışma, yeni insanlarla flört, yeni insanlarla arkadaşlık, yeni insanlarla tanışma uygulaması, yeni insanlarla flört uygulaması, yeni insanlarla arkadaşlık uygulaması">
+    <meta name="author" content="NewMeet">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="1 days">
+    <meta name="language" content="Turkish">
+    <meta name="rating" content="general">
+    <meta name="distribution" content="global">
+    <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="NewMeet">
+    <meta name="application-name" content="NewMeet">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#ffffff">
+    <meta property="og:title" content="NEWMEET - Tanışma Uygulaması">
+    <meta property="og:description" content="Yeni insanlarla tanışmak için hemen üye ol!">
+    <meta property="og:image" content="public/newmeet-logo.png">
+    <meta property="og:url" content="https://newmeet.com">
+    <meta property="og:site_name" content="NEWMEET">
+    <meta property="og:locale" content="tr_TR">
+    <meta property="og:type" content="website">
+    <title>NEWMEET - Tanışma Uygulaması</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" type="image/png" href="public/newmeet-logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+
+    <div class="logo">
+        <img src="public/newmeet-logo.png" alt="NewMeet Logo">
+    </div>
+
+    <div class="container">
+        <div class="header">
+            <h1>NEWMEET</h1>
+            <p>Yeni Nesil Tanışma Uygulaması</p>
+        </div>
+        <div class="main">
+            <div class="main-inner">
+                <h2>Yeni insanlarla tanışmak için hemen üye ol!</h2>
+                <img src="public/yuzuk.png" alt="Yüzük">
+                <!--<button>
+                    <span class="button-inner">Hemen İndir!</span>
+                </button> -->
+                <button id="downloadButton">
+                    Hemen İndir!
+                </button>
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
